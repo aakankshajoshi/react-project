dbb786d72e7aab6261df519315ff9c799c62da4f
Akanksha Joshi <aakanksha2195@gmail.com>
2020-02-06 15:03:43 -0800

    Updates

---
+++ precache-manifest.e9e999469a87a66ee2c87de77bd06789.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82446da9fcbc364d9d60aa5e5a83c26",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "d341ce5e9bdf7155e10f",
+    "url": "/react-project/static/css/main.fe7be13f.chunk.css"
+  },
+  {
+    "revision": "3155d5d9528c49cb7d3c",
+    "url": "/react-project/static/js/2.d87bcce2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-project/static/js/2.d87bcce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d341ce5e9bdf7155e10f",
+    "url": "/react-project/static/js/main.995916a8.chunk.js"
+  },
+  {
+    "revision": "53b941c0b0a12df79d22",
+    "url": "/react-project/static/js/runtime-main.2d312062.js"
+  },
+  {
+    "revision": "decf90af2cb1ff9ebd5cc2bc8d5c9ea7",
+    "url": "/react-project/static/media/baground.decf90af.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
